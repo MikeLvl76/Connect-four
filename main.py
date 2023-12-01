b250605d6db9480606f301e85573a8a4e90fec04
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-12-02 00:25:24 +0100

    add basic insert function + refactor get_diagonals function

--- main.py
+++ main.py
@@ -19,58 +19,81 @@ def print_board() -> None:
 
 
 def get_row(index: int) -> list[str]:
+    assert index < NUMBER_OF_ROWS and index >= 0, "Row index out of bounds"
     return board[index]
 
 
 def get_col(index: int) -> list[str]:
-    return [board[i][index] for i in range(len(board))]
+    assert index < NUMBER_OF_COLS and index >= 0, "Col index out of bounds"
+    return [board[i][index] for i in range(NUMBER_OF_ROWS)]
+
+
+def set_col(index: int, col: list[str]) -> None:
+    assert index < NUMBER_OF_COLS and index >= 0, "Col index out of bounds"
+    assert len(col) == NUMBER_OF_ROWS, "Incorrect column size"
+    for i in range(NUMBER_OF_ROWS):
+        board[i][index] = col[i]
 
 
 def get_location(i: int, j: int) -> str:
-    assert i < NUMBER_OF_ROWS, "Row index out of bounds"
-    assert j < NUMBER_OF_COLS, "Col index out of bounds"
+    assert i < NUMBER_OF_ROWS and i >= 0, "Row index out of bounds"
+    assert j < NUMBER_OF_COLS and j >= 0, "Col index out of bounds"
     return board[i][j]
 
 
-def get_diagonals(i: int, j: int, **kwargs: dict) -> dict:
+def get_diagonals(i: int, j: int) -> dict:
     length: int = min(NUMBER_OF_ROWS, NUMBER_OF_COLS)
     d1, d2, d3, d4 = [], [], [], []
 
     for k in range(length):
-        if i - k < 0 or j - k < 0:
-            break
-        d1.append(get_location(i - k, j - k))  # top left
+        if i - k >= 0 and j - k >= 0:
+            d1.append(get_location(i - k, j - k))  # top left
 
-    for k in range(length):
-        if i + k > length or j - k < 0:
-            break
-        d2.append(get_location(i + k, j - k))  # bottom left
+        if i + k < length + 1 and j + k < length:
+            d2.append(get_location(i + k, j + k))  # bottom right
 
-    for k in range(length):
-        if i - k < 0 or j + k > length - 1:
-            break
-        d3.append(get_location(i - k, j + k))  # top right
+        if i - k >= 0 and j + k < length:
+            d3.append(get_location(i - k, j + k))  # top right
+        
+        if i + k < length + 1 and j - k >= 0:
+            d4.append(get_location(i + k, j - k))  # bottom left
 
-    for k in range(length):
-        if i + k > length or j + k > length - 1:
-            break
-        d4.append(get_location(i + k, j + k))  # bottom right
+    print(d1, d2)
+    print(d3, d4)
+
+    d1_without_element = [*d1[1::]]
+    d3_without_element = [*d3[1::]]
 
     return {
         "location": (i, j, get_location(i, j)),
-        "top_left": d1,
-        "bottom_left": d2,
-        "top_right": d3,
-        "bottom_right": d4,
+        "first_diagonal": [*d1_without_element[::-1], *d2],
+        "second_diagonal": [*d3_without_element[::-1], *d4],
     }
 
 
+def check_four_alignement():
+    assert False, "TODO"
+
+
 def insert() -> None:
-    row_input = int(input("Choose row: "))
-    col_input = int(input("Choose column: "))
-    print(row_input, col_input)
+    while True:
+        index = int(input("Choose column index (from 0 to 5): "))
+        col = get_col(index)
+
+        try:
+            idx = col.index("X")
+            if idx > 0:
+                col[idx - 1] = "X"
+                set_col(index, col)
+        except ValueError:
+            print("ValueError")
+            col[len(col) - 1] = "X"
+            set_col(index, col)
+
+        print_board()
 
 
 if __name__ == "__main__":
     print_board()
-    print(dumps(get_diagonals(3, 4), indent=4))
+    print(get_diagonals(4, 5))
+    # insert()
