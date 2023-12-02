c7c0be635f04186aee50059c95f9e367047b77c4
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-12-02 14:15:30 +0100

    improve insert function

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from json import dumps
 
-p1: dict = {"name": "p1", "color": "red", "token": "R", "playing": False}
+p1: dict = {"name": "p1", "color": "red", "token": "R", "playing": True}
 
 p2: dict = {"name": "p2", "color": "yellow", "token": "Y", "playing": False}
 
 NUMBER_OF_ROWS = 7
 NUMBER_OF_COLS = 6
 board: list[list[str]] = [
-    [f"{i * 6 + j + 1}" for i in range(NUMBER_OF_COLS)] for j in range(NUMBER_OF_ROWS)
+    ["1" if i == j else "0" for i in range(NUMBER_OF_COLS)]
+    for j in range(NUMBER_OF_ROWS)
 ]
 
 
@@ -54,12 +55,9 @@ def get_diagonals(i: int, j: int) -> dict:
 
         if i - k >= 0 and j + k < length:
             d3.append(get_location(i - k, j + k))  # top right
-        
+
         if i + k < length + 1 and j - k >= 0:
             d4.append(get_location(i + k, j - k))  # bottom left
-
-    print(d1, d2)
-    print(d3, d4)
 
     return {
         "location": (i, j, get_location(i, j)),
@@ -68,8 +66,22 @@ def get_diagonals(i: int, j: int) -> dict:
     }
 
 
-def check_four_alignement():
-    assert False, "TODO"
+def is_win(array: list[str]):
+    string: str = "".join(array)
+    res: dict = {}
+    for i in range(len(string) - 1):
+        sub: str = string[i : i + 1]
+        freq: int = res.get(sub) or 0
+        res[sub]: int = freq + 1
+
+    most_frequent_name: str = ""
+    most_frequent_value: int = 0
+
+    for item in res.items():
+        if item[1] > most_frequent_value:
+            most_frequent_name, most_frequent_value = item
+
+    return most_frequent_name, most_frequent_value == 4
 
 
 def insert() -> None:
@@ -78,19 +90,43 @@ def insert() -> None:
         col = get_col(index)
 
         try:
-            idx = col.index("X")
-            if idx > 0:
-                col[idx - 1] = "X"
+            idx = col.index(p1["token"] if p1["playing"] else p2["token"])
+            opponent_token_idx = col.index(
+                p2["token"] if p1["playing"] else p1["token"]
+            )
+
+            if idx > 0 or opponent_token_idx > 0:  # both tokens exist
+                if (
+                    idx > opponent_token_idx
+                ):  # player token inserted before opponent token
+                    col[opponent_token_idx - 1] = (
+                        p1["token"] if p1["playing"] else p2["token"]
+                    )
+                else:  # player token inserted after opponent token
+                    col[idx - 1] = p1["token"] if p1["playing"] else p2["token"]
                 set_col(index, col)
+
         except ValueError:
             print("ValueError")
-            col[len(col) - 1] = "X"
+            if col[len(col) - 1] == p1["token"] if p1["playing"] else p2["token"]:
+                col[len(col) - 2] = p2["token"] if p2["playing"] else p1["token"]
+            else:
+                col[len(col) - 1] = p1["token"] if p1["playing"] else p2["token"]
             set_col(index, col)
 
         print_board()
 
+        if p1["playing"]:
+            p1["playing"], p2["playing"] = p2["playing"], p1["playing"]
+
+        elif p2["playing"]:
+            p2["playing"], p1["playing"] = p1["playing"], p2["playing"]
+
 
 if __name__ == "__main__":
     print_board()
-    print(get_diagonals(4, 5))
-    # insert()
+    # diagonals = get_diagonals(0, 0)
+    # first_diagonal = diagonals.get("first_diagonal")
+    # print(is_win(first_diagonal))
+    # print(get_diagonals(4, 5))
+    insert()
